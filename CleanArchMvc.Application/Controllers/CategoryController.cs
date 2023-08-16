c96ae466e26c2b43dc545810b7a039ac766f034f
andreaquilau <andre.aquilau@outlook.com>
2023-08-16 12:07:04 -0400

    Controllers Product and Categories

--- CleanArchMvc.Application/Controllers/CategoryController.cs
+++ CleanArchMvc.Application/Controllers/CategoryController.cs
@@ -7,14 +7,14 @@ using Microsoft.AspNetCore.Mvc;
 namespace CleanArchMvc.Application.Controllers;
 
 [ApiController]
-[Route("[controller]/api")]
+[Route("api/v1/[controller]")]
 public class CategoryController : ControllerBase
 {
     private readonly ILogger<CategoryController> _logger;
     private readonly ICategoryService _categoryService;
 
 
-    public CategoryController(ILogger<CategoryController> logger,ICategoryService categoryService)
+    public CategoryController(ILogger<CategoryController> logger, ICategoryService categoryService)
     {
         _logger = logger;
         _categoryService = categoryService;
@@ -29,9 +29,71 @@ public class CategoryController : ControllerBase
 
             return Ok(categories);
         }
-        catch (Exception ex) 
+        catch (Exception ex)
         {
-            return BadRequest(new { Message = ex.Message });
+            return BadRequest(new { Message = ex.InnerException?.Message });
         }
     }
+
+    [HttpGet(":id")]
+    public async Task<ActionResult<CategoryDTO>> GetById(int id)
+    {
+        try
+        {
+            var category = await _categoryService.GetById(id);
+
+            return Ok(category);
+        }
+        catch (Exception ex)
+        {
+            return new BadRequestObjectResult(new { Message = ex.InnerException?.Message });
+        }
+    }
+
+    [HttpPost()]
+    public async Task<ActionResult<CategoryDTO>> Create([FromBody] CategoryDTO categoryDTO)
+    {
+        try
+        {
+            var category = await _categoryService.Add(categoryDTO);
+
+            return Ok(category);
+        }
+        catch (Exception ex)
+        {
+            return new BadRequestObjectResult(new { Message = ex.InnerException?.Message });
+        }
+    }
+
+    [HttpPut()]
+    public async Task<ActionResult<CategoryDTO>> Update([FromBody] CategoryDTO categoryDTO)
+    {
+        try
+        {
+            var category = await _categoryService.Update(categoryDTO);
+
+            return Ok(category);
+        }
+        catch (Exception ex)
+        {
+            return new BadRequestObjectResult(new { Message = ex.InnerException?.Message });
+        }
+    }
+
+    [HttpDelete(":id")]
+    public async Task<ActionResult<CategoryDTO>> DeleteById(int id)
+    {
+        try
+        {
+            var category = await _categoryService.Remove(id);
+
+            return Ok(category);
+        }
+        catch(Exception ex)
+        {
+            return new BadRequestObjectResult(new { Message = ex.InnerException?.Message });
+        }
+    }
+
+
 }
---
+++ CleanArchMvc.Application/Controllers/ProductController.cs
@@ -0,0 +1,98 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.Application.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly IProductService _productService;
+
+
+        public ProductController(ILogger<ProductController> logger, IProductService productService)
+        {
+            _logger = logger;
+            _productService = productService;
+        }
+
+        [HttpGet()]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetCategories()
+        {
+            try
+            {
+                var products = await _productService.GetProducts();
+
+                return Ok(products);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Message = ex.InnerException?.Message });
+            }
+        }
+
+        [HttpGet(":id")]
+        public async Task<ActionResult<ProductDTO>> GetById(int id)
+        {
+            try
+            {
+                var products = await _productService.GetById(id);
+
+                return Ok(products);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(new { Message = ex.InnerException?.Message });
+            }
+        }
+
+        [HttpPost()]
+        public async Task<ActionResult<ProductDTO>> Create([FromBody] ProductDTO productDTO)
+        {
+            try
+            {
+                var products = await _productService.Add(productDTO);
+
+                return Ok(products);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(new { Message = ex.InnerException?.Message });
+            }
+        }
+
+        [HttpPut()]
+        public async Task<ActionResult<ProductDTO>> Update([FromBody] ProductDTO categoryDTO)
+        {
+            try
+            {
+                var category = await _productService.Update(categoryDTO);
+
+                return Ok(category);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(new { Message = ex.InnerException?.Message });
+            }
+        }
+
+        [HttpDelete(":id")]
+        public async Task<ActionResult<ProductDTO>> DeleteById(int id)
+        {
+            try
+            {
+                var category = await _productService.Remove(id);
+
+                return Ok(category);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(new { Message = ex.InnerException?.Message });
+            }
+        }
+
+    }
+}
